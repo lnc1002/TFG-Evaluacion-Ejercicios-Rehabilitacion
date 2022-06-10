96d4d57dbdedb7da7393c8dbafd548529c4e0147
luciianuc <luciianuc@gmail.com>
2022-06-10 13:28:37 +0200

    Funcionalidades sobre la app

--- app/MiddleWindow.py
+++ app/MiddleWindow.py
@@ -1,14 +1,18 @@
 """
-Created on Wed Jun x 2019
+Created on Wed Jun x 2022
+
 @author: Lucía Núñez
 """
 
+#from curses import window
+from doctest import master
 from email.mime import image
 import os
 from sre_parse import State
 from turtle import width
 import cv2
-#import imutils
+from cv2 import VideoCapture
+import imutils
 import time
 import tkinter.messagebox
 from tkinter import *
@@ -18,148 +22,119 @@ from PIL import Image, ImageTk
 
 from tkinter import ttk
 from tkvideo import tkvideo
+from tkVideoPlayer import TkinterVideo
 #from pygame import mixer
 import imageio
 
-
-
 class MiddleWindow():
     """
-    Clase mediadora
+    Clase mediadora. Esta clase se encarga de la comunicación entre ...
     """
     def __init__(self, window):
         self.window = window 
 
-    def showExample(self,filedirectory,playbox):
+    def showExample(self,filedirectory,playbox,value,path):
         """
-        Sirve para mostrar los videos sin la ruta completa 
+        Sirve para mostrar los videos sin la ruta completa S
         """
-        for f in filedirectory: 
-           playbox.insert("end", f"{os.path.basename(f).split('.')[0]}") 
+        for i,f in enumerate(filedirectory): 
+            playbox.insert("end", f"{os.path.basename(f).split('.')[0]}")
+            if value == 'S':
+                self.window.playlistSpecific.insert(i,path+f)
+            else:
+                self.window.playlistFull.insert(i,path+f)
+
+
 
     def addPlayList(self,filename):
         self.window.playlist.insert(self.window.index, filename)
-        
+    
+    def updateMode(self):
+        ""
 
     def simpletoggle(self):
         """
-        Esta función es para cuando se clica sobre él, que en el fondo valdría para modificar los valores 
-        Si el modo ejemplo está activo, se mostrarán una serie de vídeos y se bloquearán las casillas correspondientes a los frames
-        Cada vez que se pulse este botón tienen que cambiar el estado de las casillas
+        Esta función sirve para cambiar el modo de la aplicación.
+        Al seleccionar sobre el botón "ON/OFF" se deben modificar ciertos botones como:
+            1) En el modo ON las casillas correspondientes a los valores de los frames estarán bloqueadas
+            2) Los videos mostrados en cada modo son diferentes 
         """
         if self.window.togglebutton.config('text')[-1] == 'ON':
-            #En caso de cambiar de ON a OFF se bloquean las casillas de frame
-            self.window.textFrameInicio.config(state="normal")
-            self.window.textFrameFinal.config(state="normal")
-            #También se cambia las casillas para mostrar otros ejemplos
-            #Vaciar lista 
+            #Si existe algún vídeo cargado lo elimina
+            self.delVideo()
+            #Desbloquean las casillas de frame
+            self.window.textFrameBegin.config(state="normal")
+            self.window.textFrameEnd.config(state="normal")
+            #Se vacían las listas de vídeos
             self.window.playvideo1box.delete(0,END)
             self.window.playvideo2box.delete(0,END)
-            self.showExample(os.listdir('VideosConcretos_Ej_OFF'),self.window.playvideo1box)
-            self.showExample(os.listdir('VideosCompletos_Ej_OFF'),self.window.playvideo2box)
+            #Muestra los vídeos correspondientes 
+            self.showExample(os.listdir('VideosConcretos_Ej_OFF'),self.window.playvideo1box,'S',os.path.abspath('VideosConcretos_Ej_OFF')+'\\')
+            self.showExample(os.listdir('VideosCompletos_Ej_OFF'),self.window.playvideo2box,'F',os.path.abspath('VideosCompletos_Ej_OFF')+'\\')
             self.window.togglebutton.config(text='OFF')
 
             
         else:
-            self.window.textFrameInicio.config(state="disabled",background="red")
-            self.window.textFrameFinal.config(state="disabled",background="red")
-            #Vaciar lista 
+            self.delVideo()
+            self.window.textFrameBegin.config(state="disabled",background="red")
+            self.window.textFrameEnd.config(state="disabled",background="red")
+            #Se vacían las listas de vídeos 
             self.window.playvideo1box.delete(0,END)
             self.window.playvideo2box.delete(0,END)
-            #¿Se tendrá que actualizar la lista?
-            self.showExample(os.listdir('VideosConcretos_Ej_ON'),self.window.playvideo1box)
-            self.showExample(os.listdir('VideosCompletos_Ej_ON'),self.window.playvideo2box)
+            #Muestra los vídeos correspondientes
+            self.showExample(os.listdir('VideosConcretos_Ej_ON'),self.window.playvideo1box,'S',os.path.abspath('VideosConcretos_Ej_ON')+'\\')
+            self.showExample(os.listdir('VideosCompletos_Ej_ON'),self.window.playvideo2box,'F',os.path.abspath('VideosCompletos_Ej_ON')+'\\')
             self.window.togglebutton.config(text='ON')
 
 
-
-    def addplayVideo(self):
-        #print(self.window.playvideo1box.curselection())
-        index = self.window.playvideo1box.curselection()[0]
-        player = tkvideo(self.window.playlistConcretos[index], self.window.showVideo1, loop=1)
-
-
-    #Vamos a hacer una función que clicando sobre el vídeo lo muestre en la casilla habilitada para ello
-
-    def visualizar2(self,cap):
-        #self.window.cap
-        if self.window.cap is not None:
-            ret, frame = self.window.cap.read()
-            if ret == True:
-                #frame = imutils.resize(frame, width=640)
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                im = Image.fromarray(frame)
-                img = ImageTk.PhotoImage(image=im)
-                self.window.showVideo1.configure(image=img)
-                self.window.showVideo1.image = img
-                self.window.showVideo1.after(10, self.visualizar())
-            else:
-                self.window.showVideo1.image = ""
-                self.window.cap.release()
-
-
-
-
-
-
-
-
-
-
-
-    def visualizar(self):
-        #self.window.cap
-        if self.window.cap is not None:
-            ret, frame = self.window.cap.read()
-            if ret == True:
-                #frame = imutils.resize(frame, width=640)
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                im = Image.fromarray(frame)
-                img = ImageTk.PhotoImage(image=im)
-                self.window.showFile1.configure(image=img)
-                self.window.showFile1.image = img
-                self.window.showFile1.after(10, self.visualizar())
-            else:
-                self.window.showFile1.image = ""
-                self.window.cap.release()
-
-    def add_files(self):
+    def delVideo(self):
         """
-        Será la encargada de cargar los archivos
+        Se tiene que borrar el vídeo que se está reproduciendo
         """
-        
-        if self.window.cap is not None:
-            self.window.showFile1.image = ""
-            self.window.cap.release()
-            self.window.cap = None
-        video_path = filedialog.askopenfilename(filetypes = [
-            ("all video format", ".mp4"),
-            ("all video format", ".avi")])
-        if len(video_path) > 0:
-            #lblInfoVideoPath.configure(text=video_path)
-            self.window.cap = cv2.VideoCapture(video_path)
-            self.visualizar()
+        self.window.showVideo1.destroy()
+        self.showVideo1 = TkinterVideo(master=self.window.root, scaled=True)
+        self.showVideo1.place(relx=0.05,rely=0.4,relheight=0.45,relwidth=0.2)
+
+        self.window.showVideo2.destroy()
+        self.showVideo2 = TkinterVideo(master=self.window.root, scaled=True)
+        self.showVideo2.place(relx=0.3,rely=0.4,relheight=0.45,relwidth=0.2)
+
+        #Añadir las del video resultado
+
+    def addplayVideo(self,value):
+        if value == 1:
+            index=self.window.playvideo1box.curselection()[0]
+            self.window.showVideo1.load(self.window.playlistSpecific[index])
+            self.window.showVideo1.play()
+        else:
+            index2 = self.window.playvideo2box.curselection()[0]
+            self.window.showVideo2.load(self.window.playlistFull[index2])
+            self.window.showVideo2.play()
 
 
+    def playVideo(self,value):
+        """
+        """
+        if value == 1:
+            self.window.showVideo1.play()
+        else:
+            self.window.showVideo2.play()
+        #Else, play video resultado
 
-    def play_files(self,files):
-        
-        #No se si hacerlo asi
-        self.window.playfile1.insert(self.window.index, files[1])
-        self.window.playfile2.insert(self.window.index, files[2])
-    
-    def play_video(self):
-        if self.window.paused:
-            ""
+    def pauseVideo(self,value):
+        """
+        Pausa el vídeo
+        """
+        if value == 1: 
+            self.window.showVideo1.pause()
+        else:
+            self.window.showVideo2.pause()
+        #Else, pause video resultado
 
-    def stop_video(self):
+    def stopVideo(self,value):
         ""
-
-    def pause_video(self):
-        ""
-    
-    def rewind_music(self):
-        ""
-    
-    def on_closing(self):
-        ""
+        if value == 1:
+            self.window.showVideo1.stop()
+        elif value==2:
+            self.window.showVideo2.stop()
+        #Else, stop video resultado
